200ec24fb3f027e0911b6300f71fa91bc54d6bf7
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-04-28 23:56:34 +0600

    ads
    sad

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache({ stdTTL: 600 }); // Cache TTL set to 10 minutes
 
 export default async function handler(req, res) {
-  let { search, query, per_page, category, brand, max_price	,min_price, orderby , slug, include} = req.query;
+  let { search, query, per_page, category, brand, max_price, min_price, orderby, slug, include } = req.query;
 
   // Build axios parameters object
   let order = orderby === 'price-desc' ? 'asc' : 'desc';
@@ -16,13 +19,13 @@ export default async function handler(req, res) {
     per_page: per_page || 40,
     status: 'publish',
     search: search,
-    category,
-    min_price: min_price,
-    max_price: max_price,
+    category: category || '',
+    min_price: min_price || '',
+    max_price: max_price || '',
     orderby,
     order,
-    slug: slug,
-    include: include
+    slug: slug || '',
+    include: include || '',
   };
 
   // Add brand attribute if provided
@@ -31,13 +34,23 @@ export default async function handler(req, res) {
     params.attribute = 'pa_brand';
   }
 
+  const cacheKey = JSON.stringify(params); // Create a cache key based on the params object
+
+  // Check if the data is already cached
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) {
+    return res.status(200).json(cachedData);
+  }
+
   try {
     const response = await axios.get(`${process.env.shopLink}${query ? query : 'products'}`, {
       params,
     });
 
     const data = response.data;
-      console.log('ok')
+
+    cache.set(cacheKey, data); // Cache the data
+
     return res.status(200).json(data);
   } catch (error) {
     console.error(error);
