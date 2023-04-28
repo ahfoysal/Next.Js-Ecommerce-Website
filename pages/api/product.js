9dc3d4e8b043fdc961a023feae77c7db25be1910
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-04-29 00:05:38 +0600

    asd
    das

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,11 +1,29 @@
 import axios from 'axios';
 import NodeCache from 'node-cache';
 
-const cache = new NodeCache({ stdTTL: 600 }); // Cache TTL set to 10 minutes
+const cache = new NodeCache({ stdTTL: 120 });
+
+
 
 export default async function handler(req, res) {
-  let { search, query, per_page, category, brand, max_price, min_price, orderby, slug, include } = req.query;
+  let { search, query, per_page, category, brand, max_price	,min_price, orderby , slug, include} = req.query;
 
+  let cacheKey;
+
+  if (query) {
+    cacheKey = `${process.env.shopLink}${query}-${JSON.stringify(req.query)}`;
+  } else {
+    cacheKey = `${process.env.shopLink}products-${JSON.stringify(req.query)}`;
+  }
+
+  // Check if response is already cached
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) {
+    console.log('Serving from cache.');
+    return res.status(200).json(cachedData);
+  }
+
+  
   // Build axios parameters object
   let order = orderby === 'price-desc' ? 'asc' : 'desc';
 
@@ -13,33 +31,25 @@ export default async function handler(req, res) {
     orderby = 'price';
   }
 
+
   const params = {
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     per_page: per_page || 40,
     status: 'publish',
-    search: search,
-    category: category || '',
-    min_price: min_price || '',
-    max_price: max_price || '',
+    search,
+    category,
+    min_price: min_price,
+    max_price: max_price,
     orderby,
     order,
-    slug: slug || '',
-    include: include || '',
+    slug: slug,
+    include: include
   };
-
   // Add brand attribute if provided
   if (brand) {
     params.attribute_term = brand;
     params.attribute = 'pa_brand';
-  }
-
-  const cacheKey = JSON.stringify(params); // Create a cache key based on the params object
-
-  // Check if the data is already cached
-  const cachedData = cache.get(cacheKey);
-  if (cachedData) {
-    return res.status(200).json(cachedData);
   }
 
   try {
@@ -49,7 +59,10 @@ export default async function handler(req, res) {
 
     const data = response.data;
 
-    cache.set(cacheKey, data); // Cache the data
+    // Add response to cache with a TTL of 2 minutes (120 seconds)
+    cache.set(cacheKey, data, 120);
+
+    console.log('Adding data to cache.');
 
     return res.status(200).json(data);
   } catch (error) {
